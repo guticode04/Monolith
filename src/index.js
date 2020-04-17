1c098d33c9319456c3a2edf9875bac40696e14ca
Gustavo Gutierrez <gusguti04@gmail.com>
2020-04-16 20:29:32 -0700

    Optimize canvas

--- src/index.js
+++ src/index.js
@@ -7,5 +7,15 @@ document.addEventListener("DOMContentLoaded", ()  => {
   canvas.height = GAME_HEIGHT;
   const ctx = canvas.getContext("2d");
   const game = new Game(canvas, ctx);
-  //const gameLoop = new GameLoop(game, canvas);
+  // const gameLoop = new GameLoop(game, canvas);
+  // const bcanvas = document.getElementById("background-layer");
+  // const gcanvas = document.getElementById("game_layer");
+  // const ucanvas = document.getElementById("ui_layer");
+  // bcanvas.width = GAME_WIDTH;
+  // bcanvas.height = GAME_HEIGHT;
+  // gcanvas.width = GAME_WIDTH;
+  // gcanvas.height = GAME_HEIGHT;
+  // ucanvas.width = GAME_WIDTH;
+  // ucanvas.height = GAME_HEIGHT;
+
 })
