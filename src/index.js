b17e3c8ef0a2a60819af868691c8d8d88057d99c
Gustavo Gutierrez <gusguti04@gmail.com>
2020-04-16 20:39:20 -0700

    Clean up code

--- src/index.js
+++ src/index.js
@@ -8,14 +8,5 @@ document.addEventListener("DOMContentLoaded", ()  => {
   const ctx = canvas.getContext("2d");
   const game = new Game(canvas, ctx);
   // const gameLoop = new GameLoop(game, canvas);
-  // const bcanvas = document.getElementById("background-layer");
-  // const gcanvas = document.getElementById("game_layer");
-  // const ucanvas = document.getElementById("ui_layer");
-  // bcanvas.width = GAME_WIDTH;
-  // bcanvas.height = GAME_HEIGHT;
-  // gcanvas.width = GAME_WIDTH;
-  // gcanvas.height = GAME_HEIGHT;
-  // ucanvas.width = GAME_WIDTH;
-  // ucanvas.height = GAME_HEIGHT;
 
 })
