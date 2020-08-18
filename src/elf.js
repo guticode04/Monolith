b3c1d8f67c5dc12737d0c6a5fb985a50b3c0d606
Gustavo Gutierrez <gusguti04@gmail.com>
2020-08-17 22:38:04 -0700

    Add to elf animation

--- src/elf.js
+++ src/elf.js
@@ -1,3 +1,8 @@
+import {
+  GAME_WIDTH,
+  GAME_HEIGHT
+} from "./settings";
+
 const ELF_WIDTH = 300;//80
 const ELF_HEIGHT = 200;//110
 const ELF_POS_X = 425;//425;
@@ -6,22 +11,28 @@ class Elf {
   constructor(ctx){
     this.ctx = ctx;
     this.elf =  new Image();
-    this.elf.onload = () => { this.drawElf() };
+    this.elf.onload = () => { this.drawElfFrame() };
     this.elf.src = "./dist/assets/images/elf/elf_03_idle.png";
     this.animate();
   }
   
-  drawElf() {
+  //this is one frame of the image
+
+  drawElfFrame() {
     this.ctx.drawImage(this.elf,
     //source position
     0, 0, this.elf.width/6, this.elf.height,
     //destination position
     ELF_POS_X, ELF_POS_Y, ELF_WIDTH, ELF_HEIGHT);
   } 
+  step() {
+    let nextFrame = 0;
+    
+    return nextFrame;
+  }
 
   animate() {
-    //I really really like coding! I can create just about anything I 
-    //can think of! It's my tool for creation. Bring something to life!!
+    this.ctx.clearRec(0,0,GAME_WIDTH,GAME_HEIGHT);
 
     //We need to flip through the sprite image
     this.elf
