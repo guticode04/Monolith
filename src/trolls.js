9a2ff05f2234b861853b7603766c038461b92cd0
Gustavo Gutierrez <gusguti04@gmail.com>
2020-08-23 15:08:54 -0700

    Update troll settings and imports

--- src/trolls.js
+++ src/trolls.js
@@ -1,3 +1,8 @@
+import {
+  GAME_WIDTH,
+  GAME_HEIGHT
+} from "./settings";
+
 const TROLL_WIDTH = 230;
 const TROLL_HEIGHT = 200;
 
@@ -6,43 +11,36 @@ class Troll {
     this.ctx = ctx;
     this.troll = new Image();
     this.troll.onload = () => {
-      this.drawTroll();
+      //this will draw it as soon as there is an instance of it
+      this.drawTrollFrame();
     }
     this.troll.src = './dis/assets/images/trolls/troll_01_jump.png';
-    this.drawTroll(); //this will draw it as soon as there is an instance of it
   }
 
-  
-  // animateTroll(step) {
-  //   console.log("inside animate troll");
-
-  //   this.game.ctx.clearRect(0,0,this.game.canvas.width, this.game.canvas.height);
-  //   this.drawTroll(step);
-  //   step += 1;
-  //   if(step >= this.nextFrame.length){
-  //       step = 0;
-  //     }
-  //   // window.requestAnimationFrame(this.animateTroll.bind(this));
-  // }
-  drawTroll() {
+  animate() {
+    this.ctx.clearRect(0,0,GAME_WIDTH,GAME_HEIGHT);
+    //this could be a problem because in the game file you are calling
+    //animate over and over so each time you are defining these variables
+    //so your frame is always going to be on the first frame of the 
+    //sprite sheet
+    let currentFrameIdx = 0;
+  }
+  drawTrollFrame(frameIdx) {
     console.log("inside draw troll");
     //sheetW = 9600;
     //sheetH = 1000;
     //each frame is 1600 * 1000
-    this.troll = new Image();
-    this.troll.src = './dist/assets/images/trolls/troll_01_jump.png';
     const frameW = this.troll.width / 6;
     const frameH = this.troll.height;
-    this.troll.onload = () => {
-      // this.drawTroll();
-      this.ctx.drawImage(this.troll,
-        //source position
-        // frameW * frameStep, 0, frameW, frameH,
-        0, 0, frameW, frameH,
-        //destination position
-        0, 100, TROLL_WIDTH, TROLL_HEIGHT
-        )
-    }
+    
+    // this.drawTroll();
+    this.ctx.drawImage(this.troll,
+      //source position
+      // frameW * frameStep, 0, frameW, frameH,
+      frameIdx, 0, frameW, frameH,
+      //destination position
+      0, 100, TROLL_WIDTH, TROLL_HEIGHT);
+  }
 
     //first troll image
 
@@ -85,7 +83,7 @@ class Troll {
     //   //destination position
     //   250, 320, TROLL_WIDTH, TROLL_HEIGHT
     // )
-  }
+
 
   checkElfCollision() {
 
@@ -99,13 +97,7 @@ class Troll {
   processDamage() {
     
   }
-  
-  // //animate/traversal
-  // animateTroll() {
 
-  // }
-  
-  
   //troll settings
   
   trollHealth() {
